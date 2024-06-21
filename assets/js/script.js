e333c475068b1d6d78b53dbcc33955ba9a99eff1
BobbyValet304 <jalenwilliams90@yahoo.com>
2024-06-21 15:54:51 -0400

    adds modal with inputs, text area, close button, add task button, and console logs inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,15 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+function saveTask(task) {
+  // Todo: create a new task object
+  let newTask = {};
+  // Todo: save the new task object to localStorage
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  localStorage.setItem("nextId", JSON.stringify(nextId));
+  // Todo: re-render the task list
+  renderTaskList();
+}
 // Todo: create a function to generate a unique task id
 function generateTaskId() {}
 
@@ -12,7 +21,22 @@ function createTaskCard(task) {}
 function renderTaskList() {}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event) {}
+function handleAddTask(event) {
+  let taskTitleInput = $("#task-title-input");
+  let taskDateInput = $("#task-date-input");
+  let taskDescriptionInput = $("#task-description-input");
+  console.log(taskTitleInput.val());
+  console.log(taskDateInput.val());
+  console.log(taskDescriptionInput.val());
+  let newTask = {
+    id: generateTaskId(),
+    title: taskTitleInput.val(),
+    dueDate: taskDateInput.val(),
+    description: taskDescriptionInput.val(),
+  };
+  saveTask(newTask);
+  renderTaskList();
+}
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event) {}
@@ -21,4 +45,7 @@ function handleDeleteTask(event) {}
 function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {});
+$(document).ready(function () {
+  let addTaskButton = $("#add-task-button");
+  addTaskButton.on("click", handleAddTask);
+});
