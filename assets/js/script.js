bd3127e26ad7cc6e304453d6f7e2e38ebfc7f4b2
BobbyValet304 <jalenwilliams90@yahoo.com>
2024-06-22 11:10:18 -0400

    adds colors to cards based on due date, saves to local storage, displays cards on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,80 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+let nextId = JSON.parse(localStorage.getItem("nextId")) || 0;
 
 function saveTask(task) {
-  // Todo: create a new task object
-  let newTask = {};
   // Todo: save the new task object to localStorage
+  taskList.push(task);
+  // Todo: save the taskList and nextId to
   localStorage.setItem("tasks", JSON.stringify(taskList));
   localStorage.setItem("nextId", JSON.stringify(nextId));
   // Todo: re-render the task list
   renderTaskList();
 }
 // Todo: create a function to generate a unique task id
-function generateTaskId() {}
+function generateTaskId() {
+  return nextId++;
+}
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {}
+function createTaskCard(task) {
+  console.log(dayjs());
+  let today = dayjs();
+  cardColor = "";
+  console.log(task.dueDate);
+  console.log(today);
+  if (today.isAfter(task.dueDate, "day")) {
+    console.log("This task is overdue");
+    cardColor = "bg-danger";
+  }
+  if (today.isSame(task.dueDate, "day")) {
+    cardColor = "bg-warning";
+  }
+  if (today.isBefore(task.dueDate, "day")) {
+    cardColor = "bg-success";
+  }
+
+  return `
+    <div class="card ${cardColor}" id="${task.id}">
+      <div class="card-body">
+        <h5 class="card-title">${task.title}</h5>
+        <p class="card-text">${task.description}</p>
+        <p class="card-text">${task.dueDate}</p>
+      </div>
+    </div>
+    `;
+}
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {}
+function renderTaskList() {
+  $("#todo-cards").empty();
+  for (let index = 0; index < taskList.length; index++) {
+    //console.log(taskList[index]);
+    const createCard = createTaskCard(taskList[index]);
+    //console.log(createCard);
+    $("#todo-cards").append(createCard);
+  }
+}
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event) {
+  event.preventDefault();
   let taskTitleInput = $("#task-title-input");
   let taskDateInput = $("#task-date-input");
   let taskDescriptionInput = $("#task-description-input");
-  console.log(taskTitleInput.val());
-  console.log(taskDateInput.val());
-  console.log(taskDescriptionInput.val());
+  //console.log(taskTitleInput.val());
+  //console.log(taskDateInput.val());
+  //console.log(taskDescriptionInput.val());
   let newTask = {
     id: generateTaskId(),
     title: taskTitleInput.val(),
     dueDate: taskDateInput.val(),
     description: taskDescriptionInput.val(),
   };
+  taskTitleInput.val("");
+  taskDateInput.val("");
+  taskDescriptionInput.val("");
   saveTask(newTask);
-  renderTaskList();
 }
 
 // Todo: create a function to handle deleting a task
@@ -48,4 +87,5 @@ function handleDrop(event, ui) {}
 $(document).ready(function () {
   let addTaskButton = $("#add-task-button");
   addTaskButton.on("click", handleAddTask);
+  renderTaskList();
 });
